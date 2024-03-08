1ee656f59f7298d26621b31a97f32259c9a25457
Dóra Tokai <tokaidora88@gmail.com>
2024-03-08 12:16:06 +0200

    Fix map function in CountriesDetail

--- src/app/countries/CountriesDetail.tsx
+++ src/app/countries/CountriesDetail.tsx
@@ -16,17 +16,15 @@ const CountriesDetail = (props: IProps) => {
   return (
     <div>
       <h1>Country Detail</h1>
-      {props.countries.map((country) => {
-        return (
-          <div key={country.name}>
-            <h2>{country.name}</h2>
-            <p>Capital: {country.capital}</p>
-            <p>Population: {country.population}</p>
-            <p>Area: {country.area}</p>
-            <Image src={country.flag} alt={country.name} width="300" height="400" />
-          </div>
-        );
-      })}
+      {props.countries.map((country) => (
+        <div key={country.name}>
+          <h2>{country.name}</h2>
+          <p>Capital: {country.capital}</p>
+          <p>Population: {country.population}</p>
+          <p>Area: {country.area}</p>
+          <Image src={country.flag} alt={country.name} width="300" height="400" />
+        </div>
+      ))}
     </div>
   );
 };
